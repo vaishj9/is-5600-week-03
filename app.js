ce7efb493ff15dbd077973d2ae2747faaa0177d6
vaishj9 <vaishnavi.joshi@slu.edu>
2026-04-05 14:06:31 +0000

    Week3_Lab3_Vaishnavi_Joshi

---
+++ app.js
@@ -0,0 +1,99 @@
+const express = require('express');
+const path = require('path');
+const EventEmitter = require('events');
+
+const port = process.env.PORT || 3000;
+const app = express();
+const chatEmitter = new EventEmitter();
+
+// serve static files from public folder
+app.use(express.static(__dirname + '/public'));
+
+/**
+ * Responds with plain text
+ * 
+ * @param {http.IncomingMessage} req
+ * @param {http.ServerResponse} res
+ */
+function respondText(req, res) {
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('hi');
+}
+
+/**
+ * Responds with JSON
+ * 
+ * @param {http.IncomingMessage} req
+ * @param {http.ServerResponse} res
+ */
+function respondJson(req, res) {
+  res.setHeader('Content-Type', 'application/json');
+  res.end(JSON.stringify({ text: 'hi', numbers: [1, 2, 3] }));
+}
+
+/**
+ * Responds with a 404 not found
+ * 
+ * @param {http.IncomingMessage} req
+ * @param {http.ServerResponse} res
+ */
+function respondNotFound(req, res) {
+  res.writeHead(404, { 'Content-Type': 'text/plain' });
+  res.end('Not Found');
+}
+
+// Responds with echo JSON
+ 
+function respondEcho(req, res) {
+  const { input = '' } = req.query;
+
+  res.json({
+    normal: input,
+    shouty: input.toUpperCase(),
+    charCount: input.length,
+    backwards: input.split('').reverse().join(''),
+  });
+}
+
+//Serves chat.html
+
+function chatApp(req, res) {
+  res.sendFile(path.join(__dirname, '/chat.html'));
+}
+
+//Receives chat message
+
+function respondChat(req, res) {
+  const { message } = req.query;
+  chatEmitter.emit('message', message);
+  res.end();
+}
+
+//Server Sent Events endpoint
+
+function respondSSE(req, res) {
+  res.writeHead(200, {
+    'Content-Type': 'text/event-stream',
+    'Connection': 'keep-alive',
+  });
+
+  const onMessage = message => res.write(`data: ${message}\n\n`);
+  chatEmitter.on('message', onMessage);
+
+  res.on('close', () => {
+    chatEmitter.off('message', onMessage);
+  });
+}
+
+// Routes
+app.get('/', chatApp);
+app.get('/text', respondText);
+app.get('/json', respondJson);
+app.get('/echo', respondEcho);
+app.get('/chat', respondChat);
+app.get('/sse', respondSSE);
+
+// Start server
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
